792ab999c78dfdde4b1e264e2d2a92f4352f93ed
Adetomi <83802280+ribigbetomi@users.noreply.github.com>
2022-05-14 12:28:48 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "MacBook",
+      price: 800000,
+      image:
+        "https://freepikpsd.com/file/2020/02/Macbook-Pro-PNG-Clipart-Free.png",
+    },
+    {
+      id: "2",
+      name: "Jeep",
+      price: 5000000,
+      image:
+        "https://i.pinimg.com/originals/b0/77/5a/b0775a4b03c682798c0d9349d8ea089b.png",
+    },
+    {
+      id: "3",
+      name: "PES Pad",
+      price: 10000,
+      image:
+        "https://images.yaoota.com/NNVfpdEijES_JXgne3oeuKHfJJI=/trim/yaootaweb-production-ng/media/crawledproductimages/5d3c6eb711b7a38f434896928a35c030f49f880b.jpg",
+    },
+  ],
+};
+export default data;
